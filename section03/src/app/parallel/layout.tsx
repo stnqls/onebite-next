7a4d1a8aba1278c455ad2c32c4bd98552c5cb365
stnqls <stnqls3938@gmail.com>
2025-08-22 11:14:37 +0900

    feat: paraller route default page

--- section03/src/app/parallel/layout.tsx
+++ section03/src/app/parallel/layout.tsx
@@ -1,16 +1,25 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function Layout({
   children,
   sidebar,
+  footer,
 }: {
   children: ReactNode;
   sidebar: ReactNode;
+  footer: ReactNode;
 }) {
   return (
     <div>
+      <div>
+        <Link href={"/parallel"}>parallel</Link>
+        <Link href={"/parallel/settings"}>parallel/settings</Link>
+      </div>
+      <br />
       {sidebar}
       {children}
+      {footer}
     </div>
   );
 }
